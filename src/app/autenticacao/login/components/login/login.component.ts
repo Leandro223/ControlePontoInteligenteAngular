37f55cc76ea349a6343c985a3a25dcdbd94cfac0
Baracho <baracholss@gmail.com>
2023-05-09 10:52:51 -0300

    Feito o formulario login e inicializado no login.ts

--- src/app/autenticacao/login/components/login/login.component.ts
+++ src/app/autenticacao/login/components/login/login.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder, 
+              private router: Router, private snackBar: MatSnackBar
+              ){}
+  
+  
+  ngOnInit(){
+    
+  }
+
+  gerarForm(){
+    this.form = this.fb.group({
+      email: [''],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
+  }
+
+  logar(){
+    
+  }
 
 }
