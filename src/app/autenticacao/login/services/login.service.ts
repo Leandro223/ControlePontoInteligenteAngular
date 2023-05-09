3519676d8684700f23155b25e2e6e4981206f576
Baracho <baracholss@gmail.com>
2023-05-09 15:24:37 -0300

    Feito o metodo login post do service

--- src/app/autenticacao/login/services/login.service.ts
+++ src/app/autenticacao/login/services/login.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment as env } from 'src/app/environments/environment';
+import { Login } from '../models/login.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private readonly PATH: string = 'auth';
+
+  logar(login: Login){
+    return this.http.post(env.baseUrl + this.PATH, login );
+  }
 }
