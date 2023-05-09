f4227fdaad98309ca8b2a8bc5a4e6cc4c5de87cd
Baracho <baracholss@gmail.com>
2023-05-09 09:00:45 -0300

    Feito os imports no module login

--- src/app/autenticacao/login/login.module.ts
+++ src/app/autenticacao/login/login.module.ts
@@ -3,7 +3,14 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { LogarComponent } from './components/logar.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule} from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -13,7 +20,15 @@ import { LogarComponent } from './components/logar.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatListModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule
   ]
 })
 export class LoginModule { }
