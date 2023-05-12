88e63e08b8e2b171e3ce6e5628577888a1e76040
Baracho <baracholss@gmail.com>
2023-05-12 10:42:09 -0300

    Feito os metodos pra exibir tarefa

--- src/app/funcionario/components/lancamento/lancamento.component.ts
+++ src/app/funcionario/components/lancamento/lancamento.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Tipo } from 'src/app/shared/models/tipo.enum';
+import * as moment from 'moment';
+
+
 
 @Component({
   selector: 'app-lancamento',
   templateUrl: './lancamento.component.html',
   styleUrls: ['./lancamento.component.css']
 })
-export class LancamentoComponent {
+export class LancamentoComponent implements OnInit{
+  private dataAtualEng: string;
+  dataAtual: string;
+  geoLocation: string;
+  ultimoTipoLancado: string;
+
+  constructor(private snackbar: MatSnackBar, private router: Router){}
+  
+  
+  
+  ngOnInit(){
+    this.dataAtual = moment().format('DD-MM-YYYY HH:mm:ss');
+    this.dataAtualEng = moment().format('YYYY-MM-DD HH:mm:ss');
+    
+  }
+
+  exibirInicioTrabalho(): boolean {
+    return this.ultimoTipoLancado == '' || this.ultimoTipoLancado == Tipo.TERMINO_TRABALHO;
+  }
+
+  exibirTerminoTrabalho(): boolean{
+    return this.ultimoTipoLancado == Tipo.INICIO_TRABALHO || this.ultimoTipoLancado == Tipo.TERMINO_ALMOCO;
+  }
+
+  exibirInicioAlmoco(): boolean{
+    return this.ultimoTipoLancado == Tipo.INICIO_TRABALHO;
+  }
+
+  exibirTerminoAlmoco(): boolean{
+    return this.ultimoTipoLancado == Tipo.INICIO_ALMOCO;
+  }
+
+
 
 }
